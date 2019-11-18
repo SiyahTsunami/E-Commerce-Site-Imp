6efda2ba8f4841d5fdb0b891eca866631d0df3e4
Murat <muratkara.mk0@gmail.com>
2019-11-19 01:15:23 +0300

    Product presentation AND query class added

---
+++ src/main/java/com/dolap/backend/ecommercesite/domain/product/presentation/AddProductResponse.java
@@ -0,0 +1,4 @@
+package com.dolap.backend.ecommercesite.domain.product.presentation;
+
+public class AddProductResponse {
+}
---
+++ src/main/java/com/dolap/backend/ecommercesite/domain/product/query/FindByProductIdQuery.java
@@ -0,0 +1,4 @@
+package com.dolap.backend.ecommercesite.domain.product.query;
+
+public class FindByProductIdQuery {
+}
