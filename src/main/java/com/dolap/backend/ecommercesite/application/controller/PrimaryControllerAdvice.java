e75e45684fafe265631aa9388174d4d1547fc2c0
murat.kara <murat.kara@b2cdirect.com>
2019-11-19 18:33:38 +0300

    ControllerAdvice added

---
+++ src/main/java/com/dolap/backend/ecommercesite/application/controller/PrimaryControllerAdvice.java
@@ -0,0 +1,31 @@
+package com.dolap.backend.ecommercesite.application.controller;
+
+import com.dolap.backend.ecommercesite.domain.exception.model.ErrorDto;
+import com.dolap.backend.ecommercesite.domain.exception.model.ErrorMessage;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Collections;
+
+@RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class PrimaryControllerAdvice {
+
+    @ExceptionHandler(InvalidFormatException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ErrorDto jsonFormatException(InvalidFormatException ex) {
+        ErrorDto response = new ErrorDto();
+        response.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        response.setMessages(Collections.singletonList(new ErrorMessage(ex.getTargetType().toString(), ex.getOriginalMessage())));
+
+        return response;
+    }
+
+}
---
+++ src/main/java/com/dolap/backend/ecommercesite/application/controller/SecondaryControllerAdvice.java
@@ -0,0 +1,36 @@
+package com.dolap.backend.ecommercesite.application.controller;
+
+import com.dolap.backend.ecommercesite.domain.exception.model.ErrorDto;
+import com.dolap.backend.ecommercesite.domain.exception.model.ErrorMessage;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Collections;
+import java.util.Objects;
+import java.util.concurrent.CompletionException;
+
+@RestControllerAdvice
+@Order(Ordered.LOWEST_PRECEDENCE)
+public class SecondaryControllerAdvice {
+
+    @ExceptionHandler(Throwable.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public ErrorDto notExpectedError(Throwable throwable) {
+        if (throwable instanceof CompletionException && Objects.nonNull(throwable.getCause())) {
+            throwable = throwable.getCause();
+        }
+
+        ErrorDto response = new ErrorDto();
+        response.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        response.setMessages(Collections.singletonList(new ErrorMessage(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()), throwable.getMessage())));
+
+        return response;
+    }
+
+}
