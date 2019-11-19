c1356e22ae6e7fb295615769754fbc9df372910f
murat.kara <murat.kara@b2cdirect.com>
2019-11-19 16:45:36 +0300

    ProductTypeEnum added

---
+++ src/main/java/com/dolap/backend/ecommercesite/domain/constants/ProductTypeEnum.java
@@ -0,0 +1,10 @@
+package com.dolap.backend.ecommercesite.domain.constants;
+
+public enum  ProductTypeEnum {
+    Clothing,
+    Electronic,
+    HomeAndLife,
+    Accessory,
+    Cosmetic,
+    Supermarket
+}
