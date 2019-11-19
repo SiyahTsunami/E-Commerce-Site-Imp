c64e48ef8a6f122395b5b1282f0e79f3abd8c324
murat.kara <murat.kara@b2cdirect.com>
2019-11-19 16:46:53 +0300

    ProductController edited

--- src/main/java/com/dolap/backend/ecommercesite/application/controller/ProductController.java
+++ src/main/java/com/dolap/backend/ecommercesite/application/controller/ProductController.java
@@ -1,14 +1,16 @@
 package com.dolap.backend.ecommercesite.application.controller;
 
-import com.dolap.backend.ecommercesite.domain.product.command.AddProductCommand;
+import com.dolap.backend.ecommercesite.domain.product.Product;
+import com.dolap.backend.ecommercesite.domain.product.commands.AddProductCommand;
+import com.dolap.backend.ecommercesite.domain.product.commands.DeleteProductCommand;
+import com.dolap.backend.ecommercesite.domain.product.commands.UpdateProductCommand;
 import com.dolap.backend.ecommercesite.domain.product.presentation.AddProductResponse;
+import com.dolap.backend.ecommercesite.domain.product.query.FindByProductIdQuery;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.queryhandling.QueryGateway;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -20,16 +22,44 @@ public class ProductController {
 
     private final QueryGateway queryGateway;
 
+    @Autowired
     public ProductController(CommandGateway commandGateway, QueryGateway queryGateway) {
         this.commandGateway = commandGateway;
         this.queryGateway = queryGateway;
     }
 
     @PostMapping
-    public CompletableFuture<ResponseEntity> add(@RequestBody AddProductCommand command) {
+    public CompletableFuture<ResponseEntity> addProduct(@RequestBody AddProductCommand command) {
         CompletableFuture<AddProductResponse> task = commandGateway.send(command);
 
         return task.thenApply(ResponseEntity::ok);
+    }
+
+    @PutMapping("/{productId}")
+    public CompletableFuture<ResponseEntity> updateProduct(@RequestBody UpdateProductCommand command, @PathVariable long productId) {
+        command.setId(productId);
+
+        CompletableFuture<Void> task = commandGateway.send(command);
+
+        return task.thenApply(ResponseEntity::ok);
+    }
+
+    @DeleteMapping("/{productId}")
+    public CompletableFuture<ResponseEntity> deleteProduct(@RequestBody DeleteProductCommand command, @PathVariable long productId) {
+        command.setProductId(productId);
+
+        CompletableFuture<Void> task = commandGateway.send(command);
+
+        return task.thenApply(ResponseEntity::ok);
+    }
+
+    @GetMapping("/{productId}")
+    public CompletableFuture<ResponseEntity<Product>> findByProductId(@PathVariable long productId) {
+        FindByProductIdQuery query = new FindByProductIdQuery(productId);
+
+        CompletableFuture<Product> parcel = queryGateway.query(query, Product.class);
+
+        return parcel.thenApply(ResponseEntity::ok);
     }
 
 }
