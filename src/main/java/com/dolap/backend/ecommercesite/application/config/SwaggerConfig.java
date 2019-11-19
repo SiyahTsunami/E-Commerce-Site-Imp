0eb4f78c0390819cd9ef6b2982a8e633c7489266
murat.kara <murat.kara@b2cdirect.com>
2019-11-19 16:28:06 +0300

    SwaggerConfig added

---
+++ src/main/java/com/dolap/backend/ecommercesite/application/config/SwaggerConfig.java
@@ -0,0 +1,80 @@
+package com.dolap.backend.ecommercesite.application.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.BasicAuth;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.time.Instant;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Autowired(required = false)
+    private BuildProperties buildProperties;
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .ignoredParameterTypes(Map.class)
+                .useDefaultResponseMessages(false)
+                .forCodeGeneration(true)
+                .apiInfo(apiInfo())
+                .securitySchemes(Collections.singletonList(securityScheme()))
+                .securityContexts(Collections.singletonList(securityContext()))
+                .select().apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(applicationName)
+                .version(Optional.ofNullable(buildProperties).map(BuildProperties::getTime).orElse(Instant.now()).toString())
+                .termsOfServiceUrl(null)
+                .license(null)
+                .build();
+    }
+
+    private BasicAuth securityScheme() {
+        return new BasicAuth("basicAuth");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(Collections.singletonList(new SecurityReference("basicAuth", new AuthorizationScope[0])))
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+
+}
