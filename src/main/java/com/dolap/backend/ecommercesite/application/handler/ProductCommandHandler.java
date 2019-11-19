a6e5412a62be3420f8af69d1c6ab6e398d91aa28
murat.kara <murat.kara@b2cdirect.com>
2019-11-19 17:06:13 +0300

    ProductCommandHander and ProductQueryHandler added

--- src/main/java/com/dolap/backend/ecommercesite/application/handler/ProductCommandHandler.java
+++ src/main/java/com/dolap/backend/ecommercesite/application/handler/ProductCommandHandler.java
@@ -1,4 +1,42 @@
 package com.dolap.backend.ecommercesite.application.handler;
 
+import com.dolap.backend.ecommercesite.domain.product.Product;
+import com.dolap.backend.ecommercesite.domain.product.commands.AddProductCommand;
+import com.dolap.backend.ecommercesite.domain.product.commands.DeleteProductCommand;
+import com.dolap.backend.ecommercesite.domain.product.commands.UpdateProductCommand;
+import com.dolap.backend.ecommercesite.domain.product.presentation.AddProductResponse;
+import com.dolap.backend.ecommercesite.infrastructure.repositories.ProductCommandRespository;
+import org.axonframework.commandhandling.CommandHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ProductCommandHandler {
+
+    private final ProductCommandRespository productCommandRespository;
+
+    @Autowired
+    public ProductCommandHandler(ProductCommandRespository productCommandRespository) {
+        this.productCommandRespository = productCommandRespository;
+    }
+
+    @CommandHandler
+    public AddProductResponse add(AddProductCommand command) {
+        Product product = new Product(command);
+
+        productCommandRespository.save(product);
+
+        return new AddProductResponse(product.getId());
+    }
+
+    @CommandHandler
+    public void update(UpdateProductCommand command) {
+
+    }
+
+    @CommandHandler
+    public void delete(DeleteProductCommand command) {
+
+    }
+
 }
--- src/main/java/com/dolap/backend/ecommercesite/application/handler/ProductQueryHandler.java
+++ src/main/java/com/dolap/backend/ecommercesite/application/handler/ProductQueryHandler.java
@@ -1,4 +1,16 @@
 package com.dolap.backend.ecommercesite.application.handler;
 
+import com.dolap.backend.ecommercesite.domain.product.Product;
+import com.dolap.backend.ecommercesite.domain.product.query.FindByProductIdQuery;
+import org.axonframework.queryhandling.QueryHandler;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ProductQueryHandler {
+
+    @QueryHandler
+    public Product get(FindByProductIdQuery query) {
+        return new Product();
+    }
+
 }
