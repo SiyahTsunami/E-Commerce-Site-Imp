7b8c70308d01ffca1814e718c13e68562df55d31
murat.kara <murat.kara@b2cdirect.com>
2019-11-20 14:36:17 +0300

    findByProductId added at ProductCommandRepository

--- src/main/java/com/dolap/backend/ecommercesite/infrastructure/repositories/ProductCommandRespository.java
+++ src/main/java/com/dolap/backend/ecommercesite/infrastructure/repositories/ProductCommandRespository.java
@@ -5,6 +5,10 @@ import com.dolap.backend.ecommercesite.domain.product.Product;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductCommandRespository extends CrudRepository<Product, Long> {
+
+    Optional<Product> findProductById(long id);
 }
