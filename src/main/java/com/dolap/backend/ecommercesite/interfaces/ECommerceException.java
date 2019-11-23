918c20533b4236405664ea52c677ef3b157ab6cd
Murat <muratkara.mk0@gmail.com>
2019-11-23 17:14:42 +0300

    ECommerceException messageCode field deleted

--- src/main/java/com/dolap/backend/ecommercesite/interfaces/ECommerceException.java
+++ src/main/java/com/dolap/backend/ecommercesite/interfaces/ECommerceException.java
@@ -2,24 +2,11 @@ package com.dolap.backend.ecommercesite.interfaces;
 
 public class ECommerceException extends RuntimeException {
 
-    private String messageCode;
-
     public ECommerceException() {
     }
 
     public ECommerceException(String message) {
         super(message);
-    }
-
-    public String getMessageCode() {
-        return messageCode;
-    }
-
-    @Override
-    public String toString() {
-        return "ECommerceException{" +
-                "messageCode='" + messageCode + '\'' +
-                '}';
     }
 
 }
