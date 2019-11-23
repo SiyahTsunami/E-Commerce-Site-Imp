5d767a0f5c051789339b9127599d0fdd2c399929
Murat <muratkara.mk0@gmail.com>
2019-11-23 22:41:25 +0300

    Seller command and query tests added

---
+++ src/test/java/com/dolap/backend/ecommercesite/application/handler/SellerCommandHandlerTests.java
@@ -0,0 +1,68 @@
+package com.dolap.backend.ecommercesite.application.handler;
+
+import com.dolap.backend.ecommercesite.domain.constants.ResponseModel;
+import com.dolap.backend.ecommercesite.domain.seller.Seller;
+import com.dolap.backend.ecommercesite.domain.seller.command.AddSellerCommand;
+import com.dolap.backend.ecommercesite.domain.seller.exceptions.SellerAlreadyCreatedException;
+import com.dolap.backend.ecommercesite.domain.seller.presentation.AddSellerResponseModel;
+import com.dolap.backend.ecommercesite.infrastructure.repositories.SellerRepository;
+import com.flextrade.jfixture.JFixture;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SellerCommandHandlerTests {
+
+    @InjectMocks
+    private SellerCommandHandler sellerCommandHandler;
+
+    @Mock
+    private SellerRepository sellerRepository;
+
+    private JFixture fixture = new JFixture();
+
+    @Captor
+    private ArgumentCaptor<Seller> sellerArgumentCaptor;
+
+    @After
+    public void after() {
+        verifyNoMoreInteractions(sellerRepository);
+    }
+
+    @Test(expected = SellerAlreadyCreatedException.class)
+    public void Should_Catch_SellerAlreadyCreatedException_If_Seller_Already_Created() {
+        AddSellerCommand addSellerCommand = fixture.create(AddSellerCommand.class);
+
+        when(sellerRepository.existsSellerByUsernameAndIsDeletedFalse(addSellerCommand.getUsername())).thenReturn(true);
+
+        try {
+            sellerCommandHandler.add(addSellerCommand);
+        } finally {
+            verify(sellerRepository).existsSellerByUsernameAndIsDeletedFalse(addSellerCommand.getUsername());
+        }
+    }
+
+    @Test
+    public void Should_Add_Seller_Successfully() {
+        AddSellerCommand addSellerCommand = fixture.create(AddSellerCommand.class);
+
+        when(sellerRepository.existsSellerByUsernameAndIsDeletedFalse(addSellerCommand.getUsername())).thenReturn(false);
+
+        ResponseModel<AddSellerResponseModel> response = sellerCommandHandler.add(addSellerCommand);
+
+        verify(sellerRepository).existsSellerByUsernameAndIsDeletedFalse(addSellerCommand.getUsername());
+        verify(sellerRepository).save(sellerArgumentCaptor.capture());
+
+        Assert.assertEquals(response.getResult().getUsername(), sellerArgumentCaptor.getValue().getUsername());
+        Assert.assertEquals(sellerArgumentCaptor.getValue().getUsername(), addSellerCommand.getUsername());
+    }
+}
---
+++ src/test/java/com/dolap/backend/ecommercesite/application/handler/SellerQueryHandlerTests.java
@@ -0,0 +1,66 @@
+package com.dolap.backend.ecommercesite.application.handler;
+
+import com.dolap.backend.ecommercesite.domain.constants.ResponseModel;
+import com.dolap.backend.ecommercesite.domain.seller.Seller;
+import com.dolap.backend.ecommercesite.domain.seller.command.AddSellerCommand;
+import com.dolap.backend.ecommercesite.domain.seller.exceptions.SellerNotFoundException;
+import com.dolap.backend.ecommercesite.domain.seller.presentation.GetSellerResponseModel;
+import com.dolap.backend.ecommercesite.domain.seller.query.FindByUsernameQuery;
+import com.dolap.backend.ecommercesite.infrastructure.repositories.SellerRepository;
+import com.flextrade.jfixture.JFixture;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SellerQueryHandlerTests {
+
+    @InjectMocks
+    private SellerQueryHandler sellerQueryHandler;
+
+    @Mock
+    private SellerRepository sellerRepository;
+
+    private JFixture fixture = new JFixture();
+
+    @After
+    public void after() {
+        verifyNoMoreInteractions(sellerRepository);
+    }
+
+    @Test(expected = SellerNotFoundException.class)
+    public void Should_Catch_SellerNotFoundException_If_Seller_Not_Found() {
+        FindByUsernameQuery findByUsernameQuery = fixture.create(FindByUsernameQuery.class);
+
+        when(sellerRepository.findSellerByUsernameAndIsDeletedFalse(findByUsernameQuery.getSellerId())).thenReturn(Optional.empty());
+
+        try {
+            sellerQueryHandler.query(findByUsernameQuery);
+        } finally {
+            verify(sellerRepository).findSellerByUsernameAndIsDeletedFalse(findByUsernameQuery.getSellerId());
+        }
+    }
+
+    @Test
+    public void Should_Get_Seller_Successfully() {
+        FindByUsernameQuery findByUsernameQuery = fixture.create(FindByUsernameQuery.class);
+        Seller seller = new Seller(fixture.create(AddSellerCommand.class));
+
+        when(sellerRepository.findSellerByUsernameAndIsDeletedFalse(findByUsernameQuery.getSellerId())).thenReturn(Optional.of(seller));
+
+
+        ResponseModel<GetSellerResponseModel> response = sellerQueryHandler.query(findByUsernameQuery);
+
+        verify(sellerRepository).findSellerByUsernameAndIsDeletedFalse(findByUsernameQuery.getSellerId());
+
+        Assert.assertEquals(response.getResult().getId(), seller.getId());
+    }
+}
