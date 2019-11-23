1b1b4846f30ff9065812e40339499924b5651884
Murat <muratkara.mk0@gmail.com>
2019-11-23 17:11:06 +0300

    endpoints added to securityConfig and server port changed

--- src/main/java/com/dolap/backend/ecommercesite/application/config/SecurityConfig.java
+++ src/main/java/com/dolap/backend/ecommercesite/application/config/SecurityConfig.java
@@ -1,10 +1,11 @@
 package com.dolap.backend.ecommercesite.application.config;
 
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import static org.springframework.http.HttpMethod.*;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -12,16 +13,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity webSecurity) {
         webSecurity.ignoring()
-                .antMatchers(HttpMethod.GET, "/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/**", "/swagger-ui.html", "/webjars/**")
+                .antMatchers(GET, "/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/**", "/swagger-ui.html", "/webjars/**")
                 .antMatchers("/h2-console/**");
     }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/seller").permitAll()
+                .antMatchers(POST, "/seller").permitAll()
+                .antMatchers(GET, "/seller/**").permitAll()
+                .antMatchers(POST, "/product").permitAll()
+                .antMatchers(PUT, "/product/**").permitAll()
+                .antMatchers(DELETE, "/product/**").permitAll()
+                .antMatchers(GET, "/product/**").permitAll()
                 .anyRequest().authenticated();
 
-        http.csrf().disable(); 
+        http.csrf().disable();
     }
+
 }
