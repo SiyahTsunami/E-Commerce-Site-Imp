d4d45469920075898e8174dcff81c8e1cf48e0e0
Murat <muratkara.mk0@gmail.com>
2019-11-24 23:47:05 +0300

    Edited DateOperations

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # ECommerce-Site-Implementation
+
+This project is the e-commerce site implementation.
+
+## Tech Stack:
+- Java 8
+- Spring Boot 2.2.1
+- Axon Framework 4.2.1
+
+## Getting Started
+Running the tests:
+```
+mvn test
+```
+Running the project:
+```
+mvn spring-boot:run
+```
+
+## Swagger-UI
+You can use this endpoint:
+- localhost:8080/swagger-ui.html
+
+## H2-Console
+Configure:
+- Driver Class: org.h2.Driver
+- JDBC Url: jdbc:h2:mem:testdb
+- User name: sa
+
+You can use this endpoint:
+- localhost:8080/h2-console
+
+
+
--- src/main/java/com/dolap/backend/ecommercesite/infrastructure/operations/DateOperations.java
+++ src/main/java/com/dolap/backend/ecommercesite/infrastructure/operations/DateOperations.java
@@ -1,8 +1,6 @@
 package com.dolap.backend.ecommercesite.infrastructure.operations;
 
 import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Date;
 
 public class DateOperations {
 
@@ -10,24 +8,6 @@ public class DateOperations {
 
     public DateOperations(LocalDateTime date) {
         this.date = date;
-    }
-
-    public DateOperations(Date date) {
-        this.date = date.toInstant()
-                .atZone(ZoneId.of("Asia/Istanbul"))
-                .toLocalDateTime();
-    }
-
-    public DateOperations(String date) {
-        this.date = LocalDateTime.parse(date);
-    }
-
-    public static DateOperations of(String date) {
-        return new DateOperations(date);
-    }
-
-    public static DateOperations of(Date date) {
-        return new DateOperations(date);
     }
 
     public static String getNow() {
@@ -38,4 +18,5 @@ public class DateOperations {
     public String toString() {
         return date.toString();
     }
+
 }
